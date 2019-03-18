82bbb986ec584705d7497e02f028d132d008e4e0
gaoshuang <gao.gaoshuang@gmail.com>
2019-03-18 15:29:18 +0800

    update SDK docs

--- docs/apidoc/api_project.js
+++ docs/apidoc/api_project.js
@@ -1,15 +1,18 @@
 define({
   "name": "iWan-js-sdk",
   "version": "1.0.0",
-  "description": "iWan-js-sdk apiDoc example",
+  "description": "Wanchain public rpc js api sdk",
   "title": "iWan-js-sdk Custom apiDoc browser title",
-  "url": "https://github.com/wanchain/iWan-js-sdk",
+  "url": "wss://api.wanchain.org",
+  "template": {
+    "forceLanguage": "en"
+  },
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-03-15T09:58:28.942Z",
+    "time": "2019-03-18T07:26:14.172Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
