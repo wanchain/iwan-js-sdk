362f8185c0bf436c078a2fdb6c9e06662fcbbb14
gaoshuang <gao.gaoshuang@gmail.com>
2019-08-01 11:31:59 +0800

    merge pos-beta

--- src/libs/wsInstance.js
+++ src/libs/wsInstance.js
@@ -9,6 +9,8 @@ const CONN_OPTIONS = {
     rejectUnauthorized: false
 };
 
+const PENDING_REPONSE = {"error": "Websocket closed"};
+
 class WsEvent extends EventEmitter {}
 
 class WsInstance {
@@ -139,6 +141,7 @@ class WsInstance {
         }
         this.reTt && clearTimeout(this.reTt);
         this.reTt = setTimeout(() => {
+            this.clearPendingReq();
             this.createWebSocket();
             this.lockReconnect = false;
         }, 2000);
@@ -161,8 +164,7 @@ class WsInstance {
     }
 
     close() {
-        let that = this;
-        console.log("Websocket closed");
+        console.log("Websocket closed");      
         this.heartCheck.reset();
         if (this.wss) {
             this.wss.activeClose = true;
@@ -180,9 +182,21 @@ class WsInstance {
     getMessage(message) {
         let idx = message.id.toString()
         let fn = this.functionDict[idx];
+        if (fn != undefined) {
+            delete this.functionDict[idx];
+            fn(message);
+        } else {
+            console.log("req %s has been cleared", idx);
+        }
+    }
 
-        delete this.functionDict[idx];
-        fn(message);
+    clearPendingReq() {
+        let tempReq = this.functionDict;
+        this.functionDict = [];
+        for (let idx in tempReq) {
+            console.log("clear pending req %s", idx);
+            tempReq[idx](PENDING_REPONSE);
+        }        
     }
 }
 
