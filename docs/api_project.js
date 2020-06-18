b6490ec16982f74a9690712693de07811da63ddc
gaoshuang <gao.gaoshuang@gmail.com>
2020-06-18 10:58:32 +0800

    update version 1.2.1

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,6 +1,6 @@
 define({
   "name": "iWan-js-sdk",
-  "version": "1.2.0",
+  "version": "1.2.1",
   "description": "JavaScript SDK for iWan RPC Server",
   "title": "iWan-js-sdk Custom apiDoc browser title",
   "url": "wss://api.wanchain.org:8443",
@@ -12,8 +12,8 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "iWan",
-    "time": "2020-05-27T09:31:02.211Z",
+    "time": "2020-06-18T02:53:58.595Z",
     "url": "https://iwan.wanchain.org",
-    "version": "1.2.0"
+    "version": "1.2.1"
   }
 });
