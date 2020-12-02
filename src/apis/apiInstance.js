15db49de91f600a1e2aa747c9add197429e24803
gaoshuang <gao.gaoshuang@gmail.com>
2020-12-02 11:56:40 +0800

    add api

--- src/apis/apiInstance.js
+++ src/apis/apiInstance.js
@@ -2173,6 +2173,71 @@ class ApiInstance extends WsInstance {
 
   /**
   *
+  * @apiName estimateSmartFee
+  * @apiGroup Accounts
+  * @api {CONNECT} /ws/v3/YOUR-API-KEY estimateSmartFee
+  * @apiVersion 1.2.1
+  * @apiDescription Query a <code>'estimatesmartfee'</code> command to BTC.
+  * <br><br><strong>Returns:</strong>
+  * <br><font color=&#39;blue&#39;>«Promise,undefined»</font> Returns undefined if used with callback or a promise otherwise.
+  *
+  * @apiParam {string} chainType The chain name that you want to search, should be <code>"BTC"</code>.
+  * @apiParam {object} options Optional:
+  * <br>&nbsp;&nbsp;<code>target</code> - The numeric of confirmation target in blocks (1 - 1008).
+  * <br>&nbsp;&nbsp;<code>mode</code> - The string of fee estimate mode.
+  * @apiParam {function} [callback] Optional, the callback will receive two parameters:
+  * <br>&nbsp;&nbsp;<code>err</code> - If an error occurred.
+  * <br>&nbsp;&nbsp;<code>result</code> - The saved result.
+   * @apiIgnore Comment out this function
+  *
+  * @apiParamExample {string} JSON-RPC over websocket
+  * {"jsonrpc":"2.0","method":"estimateSmartFee","params":{"chainType":"BTC"},"id":1}
+  *
+  * @apiExample {nodejs} Example callback usage:
+  *   const ApiInstance = require('iwan-sdk');
+  *   let apiTest = new ApiInstance(YOUR-API-KEY, YOUR-SECRET-KEY);
+  *   apiTest.estimateSmartFee("BTC", (err, result) => {
+  *     console.log("Result is ", result);
+  *     apiTest.close();
+  *   });
+  *
+  * @apiExample {nodejs} Example promise usage:
+  *   const ApiInstance = require('iwan-sdk');
+  *   let apiTest = new ApiInstance(YOUR-API-KEY, YOUR-SECRET-KEY);
+  *   let result = await apiTest.estimateSmartFee("BTC");
+  *   console.log("Result is ", result);
+  *   apiTest.close();
+  *
+  * @apiSuccessExample {json} Successful Response
+  *   "success"
+  *
+  */
+ estimateSmartFee(chainType, options, callback) {
+  if (typeof(options) === "function") {
+    callback = options;
+    options = {};
+  }
+  if (!options || typeof(options) !== "object") {
+    options = {};
+  }
+  if (callback) {
+    callback = utils.wrapCallback(callback);
+  }
+  let method = 'estimateSmartFee';
+  let params = {chainType: chainType, ...options};
+
+  return utils.promiseOrCallback(callback, cb => {
+    this._request(method, params, (err, result) => {
+      if (err) {
+        return cb(err);
+      }
+      return cb(null, result);
+    });
+  });
+}
+
+  /**
+  *
   * @apiName getRegTokens
   * @apiGroup CrossChain
   * @api {CONNECT} /ws/v3/YOUR-API-KEY getRegTokens
