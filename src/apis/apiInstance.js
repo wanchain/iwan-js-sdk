dbd5ac4b489b84d25af9f64af8b3ef1f2240d445
linaly <gao.gaoshuang@gmail.com>
2021-03-02 14:51:18 +0800

    update doc

--- src/apis/apiInstance.js
+++ src/apis/apiInstance.js
@@ -9348,7 +9348,7 @@ class ApiInstance extends WsInstance {
   * @apiGroup Accounts
   * @api {CONNECT} /ws/v3/YOUR-API-KEY estimateNetworkFee
   * @apiVersion 1.3.0
-  * @apiDescription Query a <code>'estimateNetworkFee'</code> command to BTC.
+  * @apiDescription estimate network fee.
   * <br><br><strong>Returns:</strong>
   * <br><font color=&#39;blue&#39;>«Promise,undefined»</font> Returns undefined if used with callback or a promise otherwise.
   *
