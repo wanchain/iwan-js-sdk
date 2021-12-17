2ee10d4b37b313f55ffd44ff8dd336bc50829275
gaoshuang <gao.gaoshuang@gmail.com>
2021-12-17 17:57:21 +0800

    add getMinCrossChainAmount

--- src/apis/apiInstance.js
+++ src/apis/apiInstance.js
@@ -9692,6 +9692,65 @@ class ApiInstance extends WsInstance {
     });
   }
 
+  /**
+  *
+  * @apiName getMinCrossChainAmount
+  * @apiGroup CrossChain
+  * @api {CONNECT} /ws/v3/YOUR-API-KEY getMinCrossChainAmount
+  * @apiVersion 1.3.0
+  * @apiDescription Get cross chain fees.
+  * <br><br><strong>Returns:</strong>
+  * <br><font color=&#39;blue&#39;>«Promise,undefined»</font> Returns undefined if used with callback or a promise otherwise.
+  *
+  * @apiParam {string} chainType The chain being queried. Currently supports <code>"WAN"</code>, <code>"ETH"</code> and <code>"XRP"</code>.
+  * @apiParam {string} chainIds Array of chain IDs about the cross chain pair.
+  * @apiParam {function} [callback] Optional, the callback will receive two parameters:
+  * <br>&nbsp;&nbsp;<code>err</code> - If an error occurred.
+  * <br>&nbsp;&nbsp;<code>result</code> - The saved result.
+  *
+  * @apiParamExample {string} JSON-RPC over websocket
+  * {"jsonrpc":"2.0","method":"getMinCrossChainAmount","params":{"chainType":"WAN", "chainIds": ["0x8057414e", "0x8000003c"]},"id":1}
+  *
+  * @apiExample {nodejs} Example callback usage:
+  *   const ApiInstance = require('iwan-sdk');
+  *   let apiTest = new ApiInstance(YOUR-API-KEY, YOUR-SECRET-KEY);
+  *   apiTest.getMinCrossChainAmount("WAN", ["0x8057414e", "0x8000003c"], (err, result) => {
+  *     console.log("Result is ", result);
+  *     apiTest.close();
+  *   });
+  *
+  * @apiExample {nodejs} Example promise usage:
+  *   const ApiInstance = require('iwan-sdk');
+  *   let apiTest = new ApiInstance(YOUR-API-KEY, YOUR-SECRET-KEY);
+  *   let result = await apiTest.getMinCrossChainAmount("WAN", ["0x8057414e", "0x8000003c"]);
+  *   console.log("Result is ", result);
+  *   apiTest.close();
+  *
+  * @apiSuccessExample {json} Successful Response
+  *   {"lockFee":"20000000000000000000","revokeFee":"0"}
+  *
+  */
+   getMinCrossChainAmount(crossChain, symbol, options, callback) {
+     if (typeof(options) === "function") {
+       callback = options;
+       options = {};
+     }
+    if (callback) {
+      callback = utils.wrapCallback(callback);
+    }
+    let method = 'getMinCrossChainAmount';
+    let params = { crossChain: crossChain, symbol: symbol, ...options };
+
+    return utils.promiseOrCallback(callback, cb => {
+      this._request(method, params, (err, result) => {
+        if (err) {
+          return cb(err);
+        }
+        return cb(null, result);
+      });
+    });
+  }
+
 }
 
 module.exports = ApiInstance;
