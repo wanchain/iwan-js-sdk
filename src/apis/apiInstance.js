32a1ca602f8a301e69b802545ca37ea7b6aec961
linaly <gao.gaoshuang@gmail.com>
2022-05-10 18:05:25 +0800

    add getTokenPairsHash

--- src/apis/apiInstance.js
+++ src/apis/apiInstance.js
@@ -1199,6 +1199,11 @@ class ApiInstance extends WsInstance {
   * Set to <code>false</code> (the default) to return the serialized transaction as hex.
   * <br>&nbsp;&nbsp;&nbsp;&nbsp;
   * Set to <code>true</code> to return a decoded transaction.
+  * <br>&nbsp;&nbsp;<code>withTopics</code> - Whether to get the topics transaction info, in this case, the <code>chainType</code> should be <code>"TRX"</code>:
+  * <br>&nbsp;&nbsp;&nbsp;&nbsp;
+  * Set to <code>false</code> (the default) to return the common transaction.
+  * <br>&nbsp;&nbsp;&nbsp;&nbsp;
+  * Set to <code>true</code> to return a transaction with topics.
   * @apiParam {function} [callback] Optional, the callback will receive two parameters:
   * <br>&nbsp;&nbsp;<code>err</code> - If an error occurred.
   * <br>&nbsp;&nbsp;<code>result</code> - The saved result.
@@ -10035,6 +10040,66 @@ class ApiInstance extends WsInstance {
    });
  }
 
+
+/**
+*
+* @apiName getTokenPairsHash
+* @apiGroup CrossChain
+* @api {CONNECT} /ws/v3/YOUR-API-KEY getTokenPairsHash
+* @apiVersion 1.3.0
+* @apiDescription Get token pairs hash.
+* <br><br><strong>Returns:</strong>
+* <br><font color=&#39;blue&#39;>«Promise,undefined»</font> Returns undefined if used with callback or a promise otherwise.
+*
+* @apiParam {string} chainType The chain being queried. Currently supports <code>"ADA"</code>.
+* @apiParam {object} options Optional:
+* <br>&nbsp;&nbsp;<code>epochID</code> - The ID of epoch you want to search.
+* @apiParam {function} [callback] Optional, the callback will receive two parameters:
+* <br>&nbsp;&nbsp;<code>err</code> - If an error occurred.
+* <br>&nbsp;&nbsp;<code>result</code> - The saved result.
+*
+* @apiParamExample {string} JSON-RPC over websocket
+* {"jsonrpc":"2.0","method":"getTokenPairsHash","params":{},"id":1}
+*
+* @apiExample {nodejs} Example callback usage:
+*   const ApiInstance = require('iwan-sdk');
+*   let apiTest = new ApiInstance(YOUR-API-KEY, YOUR-SECRET-KEY);
+*   apiTest.getTokenPairsHash((err, result) => {
+*     console.log("Result is ", result);
+*     apiTest.close();
+*   });
+*
+* @apiExample {nodejs} Example promise usage:
+*   const ApiInstance = require('iwan-sdk');
+*   let apiTest = new ApiInstance(YOUR-API-KEY, YOUR-SECRET-KEY);
+*   let result = await apiTest.getTokenPairsHash();
+*   console.log("Result is ", result);
+*   apiTest.close();
+*
+* @apiSuccessExample {json} Successful Response
+*   e1fb2e32321423509bfa9e096cad3596
+*
+*/
+  getTokenPairsHash(options, callback) {
+    if (typeof(options) === "function") {
+      callback = options;
+      options = {};
+    }
+    if (callback) {
+      callback = utils.wrapCallback(callback);
+    }
+    let method = 'getTokenPairsHash';
+    let params = { ...options };
+
+    return utils.promiseOrCallback(callback, cb => {
+      this._request(method, params, (err, result) => {
+        if (err) {
+          return cb(err);
+        }
+        return cb(null, result);
+      });
+    });
+  }
 }
 
 module.exports = ApiInstance;
